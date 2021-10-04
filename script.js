c1b5d36f4282be90eb867a0db20e32ca1f5c65e4
Jake Swackhammer <j.100101@gmail.com>
2021-10-04 13:44:03 -0400

    fixed bug

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const renderMovieCard = (title, bodyText) => {
    let cardEl = document.createElement('div')
    let titleEl = document.createElement('h3')
    let descriptionEl = document.createElement('p')
-   console.log(titleEl)
-
-   titleEl.textContent =title
+   
+   titleEl.textContent = title
    descriptionEl.textContent =bodyText
 
-   cardEl.append(title)
+   cardEl.append(titleEl)
    cardEl.append(descriptionEl)
    
    document.querySelector('#results-container').append(cardEl)
