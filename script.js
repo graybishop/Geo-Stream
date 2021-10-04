7035e727f44f1887b63341d93b0fe97ecfddd3c7
Jake Swackhammer <j.100101@gmail.com>
2021-10-04 13:16:10 -0400

    started function

---
+++ script.js
@@ -0,0 +1,7 @@
+const renderMovieCard = (title, bodyText) => {
+   let cardEl = document.createElement('div')
+   let titleEl = document.createElement('h3')
+   let descriptionEl = document.createElement('p')
+
+
+}
