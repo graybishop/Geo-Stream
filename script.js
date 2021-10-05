0231cb2c853aa4aa8e5999576afe011f5db20b0e
BeauChewning <beauchewning96@gmail.com>
2021-10-05 11:46:45 -0400

    added basic style to html

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let searchBar = document.querySelector("#search")
 if (localStorage.getItem('recentMovieSearches') !== null) {
     storageArray = JSON.parse(localStorage.getItem('recentMovieSearches'))
 }
-// storageArray = localStorage.getItem('recentMovieSearches')
+
 
 function handleForm(event) {
     event.preventDefault()
