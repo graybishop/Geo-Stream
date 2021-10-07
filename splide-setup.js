df118f2057ea4a82c25f6df1871c9f4006721d98
Jake Swackhammer <j.100101@gmail.com>
2021-10-07 13:50:16 -0400

    fixes #89

--- splide-setup.js
+++ splide-setup.js
@@ -8,12 +8,15 @@ let gallery = new Splide( '.splide', {
     gap: '1.25rem',
     arrows: true,
     pagination: false,
+    start: 0,
 } )
 
 
 
 document.addEventListener( 'DOMContentLoaded', function() {
     gallery.mount();
+    document.querySelector('.splide').classList.add('scale-x-100')
+    document.querySelector('.splide').classList.remove('scale-x-0')
     const { track } = gallery.Components.Elements;
     gallery.Components.Style.ruleBy( track, 'overflow', 'visible');
 } );
